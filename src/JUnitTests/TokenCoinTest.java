0acad58125c21be64e61ffa9445050e455c47797
Adanos <polon1@op.pl>
2015-09-10 16:38:12 +0200

    Issue #1

--- src/JUnitTests/TokenCoinTest.java
+++ src/JUnitTests/TokenCoinTest.java
@@ -116,4 +116,95 @@ public class TokenCoinTest
       assertEquals("Fields didn't match ", field.get(tokenCoin), Color.WHITE);
 	}
 
+	@Test
+	public void testToString()
+	{
+		String result = "";
+		String text = "Żeton o wartości 0 o współrzędnych [2, 5].";
+		TokenCoin tokenCoin = new TokenCoin(2, 5, Color.red);
+		result = tokenCoin.toString();
+		
+		assertEquals("Texts are not equal ", result, text);
+	}
+	
+	@Test
+	public void testEqualsIdentity()
+	{
+		TokenCoin tokenCoin = new TokenCoin(2, 3, Color.red);
+		
+		boolean result = tokenCoin.equals(tokenCoin);
+		
+		assertEquals("No identity in method equals ", result, true);
+	}
+	
+	@Test
+	public void testEqualsSymmetry()
+	{
+		TokenCoin tokenCoin = new TokenCoin(2, 3, Color.red);
+		TokenCoin tokenCoin2 = new TokenCoin(4, 3, Color.red);
+		
+		boolean result = tokenCoin.equals(tokenCoin2);
+		boolean result2 = tokenCoin2.equals(tokenCoin);
+		
+		assertEquals("No symmetry in method equals ", result, result2);
+	}
+	
+	@Test
+	public void testEqualsTransitivity()
+	{
+		TokenCoin tokenCoin = new TokenCoin(2, 3, Color.red);
+		TokenCoin tokenCoin2 = new TokenCoin(2, 3, Color.red);
+		TokenCoin tokenCoin3 = new TokenCoin(2, 3, Color.red);
+		
+		boolean result = tokenCoin.equals(tokenCoin2);
+		boolean result2 = tokenCoin2.equals(tokenCoin3);
+		boolean result3 = tokenCoin.equals(tokenCoin3);
+		
+		assertEquals("No transitivity in method equals ", result, true);
+		assertEquals("No transitivity in method equals ", result2, true);
+		assertEquals("No transitivity in method equals ", result3, true);
+	}
+	
+	@Test 
+	public void testEqualsConsistency()
+	{
+		TokenCoin tokenCoin = new TokenCoin(2, 3, Color.red);
+		TokenCoin tokenCoin2 = new TokenCoin(2, 3, Color.red);
+		
+		boolean result = tokenCoin.equals(tokenCoin2);
+		boolean result2 = tokenCoin.equals(tokenCoin2);
+		boolean result3 = tokenCoin.equals(tokenCoin2);
+		tokenCoin2 = new TokenCoin(2, 3, Color.green);
+		boolean result4 = tokenCoin.equals(tokenCoin2);
+		boolean result5 = tokenCoin.equals(tokenCoin2);
+		
+		assertEquals("No consistency in method equals ", result, true);
+		assertEquals("No consistency in method equals ", result2, true);
+		assertEquals("No consistency in method equals ", result3, true);
+		assertEquals("No consistency in method equals ", result4, false);
+		assertEquals("No consistency in method equals ", result5, false);
+	}
+	
+	@Test
+	public void testEqualsNonNullity()
+	{
+		TokenCoin tokenCoin = new TokenCoin(1, 3, Color.red);
+		boolean result = tokenCoin.equals(null);
+		
+		assertEquals("NonNullity test in method equals is failed ", result, false);
+	}
+	
+	@Test
+	public void testHashCode()
+	{
+		TokenCoin tokenCoin = new TokenCoin(1, 3, Color.red);
+		TokenCoin tokenCoin2 = new TokenCoin(1, 3, Color.red);
+		TokenCoin tokenCoin3 = new TokenCoin(1, 3, Color.blue);
+		int result = tokenCoin.hashCode();
+		int result2 = tokenCoin2.hashCode();
+		int result3 = tokenCoin3.hashCode();
+		
+		assertEquals("Hashcode is different for equal objects ", result, result2);
+		assertNotEquals("Hashcode is equal for different objects ", result, result3);
+	}
 }
--- src/Model/TokenCoin.java
+++ src/Model/TokenCoin.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 public class TokenCoin
 {
+	private volatile int hashCode;
 	private int value;
 	private int x;
 	private int y;
@@ -65,4 +66,45 @@ public class TokenCoin
 		this.color = color;
 	}
 	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (!(obj instanceof TokenCoin)) return false;
+		TokenCoin tokenCoin = (TokenCoin) obj;
+		
+		return tokenCoin.value == value && tokenCoin.x == x && tokenCoin.y == y 
+				&& tokenCoin.color.equals(color);
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		int result = hashCode;
+		int firstPrimeNumber = 19;
+		int secondPrimeNumber = 23;
+		int modulo = 89;
+		
+		if (result == 0)
+		{
+			result = firstPrimeNumber;
+			result = (secondPrimeNumber * result + value) % modulo;
+			result = (secondPrimeNumber * result + x) % modulo;
+			result = (secondPrimeNumber * result + y) % modulo;
+			result = (secondPrimeNumber * result + color.hashCode()) % modulo;
+			
+			hashCode = result;
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String result = "Żeton o wartości " + getValue() + " o współrzędnych [" + getX() + ", " 
+							+ getY() + "].";
+		
+		return result;
+	}
 }
